b27017a971b590cefba924fa313bc10ff1bd46aa
philtothetop <philippe.baron@hotmail.com>
2016-11-01 22:39:42 -0400

    created 10x10 tile and simple cubic character

---
+++ Assets/scripts/Grid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+    public Transform tile;
+	// Use this for initialization
+	void Start () {
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/tileScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tileScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onMouseDown()
+    {
+        Object movedCharacter = GameObject.FindGameObjectWithTag("Character");
+        Debug.Log(movedCharacter);
+        Vector3 pos = transform.position;
+        Debug.Log(transform.position);
+        ((Transform)movedCharacter).position = pos;
+    }
+}
